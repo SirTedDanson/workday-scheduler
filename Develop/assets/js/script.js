61e0a4c1e6d87fdec4eb04a0676b26d6ecf57db2
sirteddanson <knowledgeMED@gmail.com>
2021-12-01 14:16:46 -0500

    initial save button logic

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,12 +1,12 @@
-
 var currentDayEl = document.getElementById("currentDay");
 var currentDay = moment();
+var scheduledTasks = [];
 
 // dynamically change amount of work hours
 var workHours = 8;
 // dynamically change schedule start. 
 // format: "HOUR(AM/PM)" 
-var workDayStart = "9AM"
+var workDayStart = "9AM";
 
 // ------------------------------ CREATE DOM ELEMENTS -----------------------------
 var createSchedule = function () {
@@ -16,16 +16,20 @@ var createSchedule = function () {
   for (i = 0; i < (workHours+1); i++) {
     // work scheduler hour due
     var hourDue = moment(workDayStart, "hA").add(i, "hour");
+    var displayedHour = hourDue.format("hA")
     // dynamically create time blocks with jquery
     var timeBlockEl = $("<div>")
       .addClass("row time-block");
     var timeBlockLabel = $("<label>")
       .addClass("col-1 hour")
-      .text(hourDue.format("hA"));
+      .text(displayedHour);
     var timeBlockText = $("<textarea>")
-      .addClass("col description");
+      .addClass("col description")
+      .attr("id", i)
+      //.text(savedTask)
     var timeBlockButton = $("<button>")
       .addClass("col-1 saveBtn");
+
     var saveIcon = $("<i>")
       .addClass("fas fa-save");
 
@@ -37,6 +41,7 @@ var createSchedule = function () {
     // check time block and color code
     auditSchedule(timeBlockEl, hourDue);
   };
+
   // periodically check time while on page
   setInterval(function() {
     auditSchedule(timeBlockEl, hourDue);
@@ -46,24 +51,52 @@ var createSchedule = function () {
 // ---------------------------- COLOR CODING FUNCTION ------------------------------
 var auditSchedule = function(timeBlock, hour) {
   // get difference in hours between current time and the time block hour
-  var timeDiff = hour.diff(currentDay)/60/60/1000;
+  var timeDiff = (hour.diff(currentDay)/60/60/1000);
 
   // remove old classes from element
-  $(timeBlock).removeClass("past present future")
+  $(timeBlock).removeClass("past present future");
 
   // highlight time block based on conditions
   // if timeblock is 1 hour or greater before the current time add class 'past'
   if (timeDiff <= -1){
-    $(timeBlock).addClass("past")
+    $(timeBlock).addClass("past");
   }
   // if timeblock is in the current hour add class 'present'
   else if (timeDiff <= 0) {
-    $(timeBlock).addClass("present")
+    $(timeBlock).addClass("present");
   }
   // if timeblock is 1 hour or greater after the current time add class 'future'
   else if (timeDiff >= 0) {
-    $(timeBlock).addClass("future")
-  }
+    $(timeBlock).addClass("future");
+  };
 };
+
+// save to local storage
+var saveSchedule = function() {
+  localStorage.setItem("schedule", JSON.stringify(scheduledTasks));
+}
+
+// on load function once DOM is fully loaded
+$(document).ready(function(){
+  // save time block text when clicking save button
+  $(".saveBtn").click(function(){
+
+    var logText = $(this).parents(".time-block").find("textarea").val();
+    var logId = $(this).parents(".time-block").find("textarea").attr("id");
+    console.log(logText);
+    console.log(logId);
+    console.log("button clicked");
+    currentSchedule = {
+      id: logId,
+      text: logText
+    };
+
+    scheduledTasks.push(currentSchedule)
+    console.log(currentSchedule)
+    console.log(scheduledTasks)
+
+    saveSchedule ();
+  });
+});
 
 createSchedule();
